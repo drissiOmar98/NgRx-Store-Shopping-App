4f9249af3b94fa1e80dfec7387e1cb8e4f211a0e
DrissiOmar <omar.drissi@esprit.tn>
2025-07-01 22:51:26 +0100

    centralize Font Awesome icons in constants file

---
+++ Shop_NgRx/src/app/shared/font-awesome-icons.ts
@@ -0,0 +1,103 @@
+import {
+  faHeart as faHeartRegular,
+  faStar as faStarRegular,
+  IconDefinition
+} from "@fortawesome/free-regular-svg-icons";
+import {
+  faBars,
+  faChevronDown,
+  faChevronLeft,
+  faChevronRight,
+  faCircleNotch,
+  faCircleXmark,
+  faHeart,
+  faHome,
+  faMagnifyingGlass,
+  faMinus,
+  faPlus,
+  faShoppingCart,
+  faStar,
+  faStarHalfStroke,
+  faUser,
+  faTruck,
+  faCreditCard,
+  faShieldHalved,
+  faExchange,
+  faPhone,
+  faEnvelope,
+  faLocationDot,
+  faFilter,
+  faCheck,
+  faTimes,
+  faTrash,
+  faSignInAlt,
+  faSignOutAlt,
+  faUserCircle,
+  faBarcode,
+  faGift,
+  faTag,
+  faPercent,
+  faBoxOpen,
+  faHeadset,
+  faReceipt
+} from "@fortawesome/free-solid-svg-icons";
+
+export const fontAwesomeIcons: IconDefinition[] = [
+  // Basic navigation
+  faHome,
+  faBars,
+  faChevronLeft,
+  faChevronRight,
+  faChevronDown,
+
+  // User related
+  faUser,
+  faUserCircle,
+  faSignInAlt,
+  faSignOutAlt,
+
+  // Shopping
+  faShoppingCart,
+  faHeart,
+  faHeartRegular,
+  faMagnifyingGlass,
+  faFilter,
+
+  // Product actions
+  faPlus,
+  faMinus,
+  faTrash,
+  faExchange,
+
+  // Ratings
+  faStar,
+  faStarRegular,
+  faStarHalfStroke,
+
+  // Product labels
+  faTag,
+  faPercent,
+  faGift,
+  faBarcode,
+
+  // Order & payment
+  faCreditCard,
+  faTruck,
+  faReceipt,
+  faBoxOpen,
+
+  // Support
+  faHeadset,
+  faPhone,
+  faEnvelope,
+  faLocationDot,
+
+  // Trust badges
+  faShieldHalved,
+
+  // UI elements
+  faCircleNotch,
+  faCircleXmark,
+  faCheck,
+  faTimes
+];
