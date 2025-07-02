3b8f8e3a2d90d8dd3070ab78209da1cf12ad13f4
DrissiOmar <omar.drissi@esprit.tn>
2025-07-02 23:02:25 +0100

    feat(Favorites/store): create favorites reducer with rehydration and state management

---
+++ Shop_NgRx/src/app/features/favorites/favorites-store/favorites.reducer.ts
@@ -0,0 +1,24 @@
+import {initialFavoritesState} from './favorites.state';
+import {on} from '@ngrx/store';
+import {FavoritesActions} from './favorites.actions';
+import {createRehydrateReducer} from '../../cart/store/create-rehydrate.reducer';
+
+export const favoritesReducer = createRehydrateReducer(
+  'favorites', // localStorage key
+  initialFavoritesState,
+  on(FavoritesActions.addToFavorites, (state, product) => {
+    // Prevent duplicates
+    const exists = state.items.some(item => item.id === product.id);
+    return exists ? state : {
+      ...state,
+      items: [...state.items, product]
+    };
+  }),
+  on(FavoritesActions.removeFromFavorites, (state, { id }) => ({
+    ...state,
+    items: state.items.filter(item => item.id !== id)
+  })),
+  on(FavoritesActions.clearFavorites, () => ({
+    items: []
+  }))
+);
